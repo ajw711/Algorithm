539c964987506061f9a6537f2bce5e32b91fff4a
ajw711 <122139496+ajw711@users.noreply.github.com>
2025-02-03 03:13:27 +0900

    [Silver I] Title: 포도주 시식, Time: 124 ms, Memory: 14996 KB -BaekjoonHub

---
+++ 백준/Silver/2156. 포도주 시식/포도주 시식.java
@@ -0,0 +1,46 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+
+
+
+
+public class Main {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static int N;
+
+    static int [] arr;
+    static int [] dp;
+
+    public static void main(String[] args) throws IOException {
+
+
+        N = Integer.parseInt(br.readLine());
+
+        arr = new int[N+1];
+        dp = new int[N+1];
+
+
+
+        for(int i=1; i<=N; i++){
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        dp[1] = arr[1];
+        if(N>=2){
+            dp[2] = dp[1] + arr[2];
+        }
+
+        for(int i=3; i<=N; i++){
+            dp[i] = Math.max(dp[i-1], Math.max(dp[i-2] + arr[i], dp[i-3] + arr[i-1] + arr[i]));
+        }
+        System.out.println(dp[N]);
+
+
+
+
+    }
+}
