fd1b43da8f8aedd0fd54ab5cb7b5efe541e9efe3
ajw711 <122139496+ajw711@users.noreply.github.com>
2025-02-07 16:03:47 +0900

    [Gold V] Title: 두 용액, Time: 344 ms, Memory: 28904 KB -BaekjoonHub

---
+++ 백준/Gold/2470. 두 용액/두 용액.java
@@ -0,0 +1,57 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+
+
+
+public class Main {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    static int N;
+    static int[] arr;
+
+
+    static int rLeft, rRight;
+    static Long sum = 1000000000L+1000000000L;
+
+
+
+
+
+    public static void main(String[] args) throws IOException {
+
+        N = Integer.parseInt(br.readLine());
+        arr = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i=0; i<N; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int start = 0;
+        int end = N-1;
+
+        Arrays.sort(arr);
+
+        while(start< end){
+             long sum2 = arr[start] + arr[end];
+
+            if(Math.abs(sum) > Math.abs(sum2)){
+                sum = sum2;
+                rLeft = arr[start];
+                rRight = arr[end];
+            }
+
+            if(sum2 <= 0){
+                start +=1;
+            }else{
+                end -=1;
+            }
+        }
+
+        System.out.println(rLeft+" "+rRight);
+
+    }
+}
