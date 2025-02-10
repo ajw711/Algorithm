1625b2e75804f96f5d8b6d2e021ff1c671e2327f
ajw711 <122139496+ajw711@users.noreply.github.com>
2025-02-10 13:46:46 +0900

    [Silver IV] Title: 암기왕, Time: 1604 ms, Memory: 232200 KB -BaekjoonHub

--- 백준/Silver/2776. 암기왕/암기왕.java
+++ 백준/Silver/2776. 암기왕/암기왕.java
@@ -3,12 +3,16 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
 
+
+
 public class Main {
 
-    static int T, N, M;
-    static int[] arr;
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-    static int binarySearch(int target) {
+    static int T, N, M;
+    static int [] arr;
+    static int binarySearch(int target){
+
         int start = 0, end = N - 1;
         while (start <= end) {
             int mid = (start + end) / 2;
@@ -19,33 +23,30 @@ public class Main {
         return 0;
     }
 
+
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
 
         T = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
 
-        for (int i = 0; i < T; i++) {
-            // 1. N 입력 및 배열 초기화
+        for(int i=0; i<T; i++){
             N = Integer.parseInt(br.readLine());
             arr = new int[N];
-
-            // 2. 배열 입력 및 정렬
             StringTokenizer st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < N; j++) arr[j] = Integer.parseInt(st.nextToken());
-            Arrays.sort(arr);
 
-            // 3. M 입력 및 탐색
+            for(int j=0; j<N; j++){
+                arr[j] = Integer.parseInt(st.nextToken());
+            }
+            Arrays.sort(arr);
             M = Integer.parseInt(br.readLine());
             st = new StringTokenizer(br.readLine());
-
-            for (int j = 0; j < M; j++) {
+            for(int j=0; j<M; j++){
                 int target = Integer.parseInt(st.nextToken());
                 sb.append(binarySearch(target)).append("\n");
             }
-        }
 
-        // 최적화된 출력 (한 번에 출력)
+        }
         System.out.print(sb);
+
     }
 }
