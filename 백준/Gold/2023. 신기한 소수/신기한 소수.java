412cef2e719e87336c3aaf483938a7a21cb12153
ajw711 <122139496+ajw711@users.noreply.github.com>
2025-07-22 17:53:09 +0900

    [Gold V] Title: 신기한 소수, Time: 96 ms, Memory: 14156 KB -BaekjoonHub

--- 백준/Gold/2023. 신기한 소수/신기한 소수.java
+++ 백준/Gold/2023. 신기한 소수/신기한 소수.java
@@ -11,7 +11,7 @@ public class Main {
     static boolean isPrime(int num) {
         if(num <2) 
             return false;
-        for(int i=2 ; i*i<=num; i++) {
+        for(int i=2 ; i<=Math.sqrt(num); i++) {
             if(num%i == 0) {
                 return false;
             }
