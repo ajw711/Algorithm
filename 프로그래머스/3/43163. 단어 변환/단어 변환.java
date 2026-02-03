cdfd48d61946d1b96e7fcfaf9e37975364e683c1
ajw711 <122139496+ajw711@users.noreply.github.com>
2026-02-03 17:50:45 +0900

    [level 3] Title: 단어 변환, Time: 0.62 ms, Memory: 79.7 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -1,53 +1,51 @@
 import java.util.*;
-
 class Solution {
     
-    static class Node{
-        private String word;
-        private int cnt;
+    static class Node {
+        String s;
+        int cnt;
         
-        public Node(String word, int cnt){
-            this.word = word;
+        public Node(String s, int cnt){
+            this.s = s;
             this.cnt = cnt;
         }
     }
     
-    private int bfs(String begin, String target, String[] words){
+    private int dfs(String begin, String target, String[] words){
         ArrayDeque<Node> dp = new ArrayDeque<>();
-        //이미 사용한 단어는 다시 안 씀
-        boolean[] visited = new boolean[words.length];
         dp.add(new Node(begin, 0));
-        
+        boolean[] visited = new boolean[words.length];
+   
         while(!dp.isEmpty()){
-            Node cur = dp.poll(); //맨 앞의 단어를 꺼냄
+            Node cur = dp.poll();
             
-            if(cur.word.equals(target)) return cur.cnt;
+            if(cur.s.equals(target)) return cur.cnt;
             
             for(int i=0; i<words.length; i++){
-                //아직 미방분 + 한글자만 다른 경우
-                if(!visited[i] && check(cur.word, words[i])){
+                if(!visited[i] && check(cur.s, words[i])){
                     visited[i] = true;
-                    dp.add(new Node(words[i], cur.cnt+1));
+                    dp.add(new Node(words[i], cur.cnt + 1));
                 }
             }
         }
         return 0;
+        
     }
     
-    private boolean check(String word, String target){
-        int diff = 0;
-        for(int i=0; i<word.length(); i++){
-            if(word.charAt(i) != target.charAt(i)){
-                diff++;
+    private boolean check(String s, String word){
+        int dif = 0;
+        for(int i=0; i<s.length(); i++){
+            if(s.charAt(i) != word.charAt(i)){
+                dif++;
             }
         }
-        return diff == 1;
+        return dif == 1;
     }
     
     public int solution(String begin, String target, String[] words) {
         int answer = 0;
-        answer = bfs(begin, target, words);
-        
+        answer = dfs(begin, target, words);
         return answer;
     }
+    
 }
